93aad7de47d28d36b9e9c788a4091cb4ad1ef8ac
Kevin Hubert <>
2018-07-15 16:18:32 +0200

    func: Add default route (404-page) if route does not exists.

--- app/src/App.js
+++ app/src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Header from './components/Header'
-
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App "><Header>
+        <div className="App ">
+          <Header>
             <div className='half'>
               <Navigation />
             </div>
           </Header>
-          <Route path='/login' exact render={() => "New"} />
-          <Route path='/' exact render={() => "/"} />
-          <Route path='/login' exact render={() => "Login"} />
-          <Route path='/new' exact render={() => "New"} />
-          <Route path='/profile' exact render={() => "Profile"} />
-          <Route path='/leaderboard' exact render={() => "Leaderboard"} />
+          <Switch>
+            <Route path='/' exact render={() => "/"} />
+            <Route path='/login' exact render={() => "Login"} />
+            <Route path='/new' exact render={() => "New"} />
+            <Route path='/profile' exact render={() => "Profile"} />
+            <Route path='/leaderboard' exact render={() => "Leaderboard"} />
+            <Route exact render={() => "Other (404)"} />
+          </Switch>
         </div>
-      </BrowserRouter>
+      </BrowserRouter >
     );
   }
 }
