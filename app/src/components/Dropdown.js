75d4decd60fc184e74275cd3b4af830a562f6836
Kevin Hubert <>
2018-07-18 23:42:47 +0200

    refactor: Removed card component. Instead of using Card-Component the Login-Component implements the render-logic itself, because the Component will not be used multiple times.

---
+++ app/src/components/Dropdown.js
@@ -0,0 +1,26 @@
+// import React, { Component } from 'react'
+// import PropTypes from 'prop-types'
+
+// class Inputfield extends Component {
+
+//   handleChange = () => {
+
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <label>{this.props.label}</label>
+//         <input type='text' placeholder= onChange={this.props.handleChange} />
+//       </div>
+//     );
+//   }
+
+// }
+
+// Inputfield.Proptypes = {
+//   items: PropTypes.array.isRequired,
+//   text: PropTypes.string.isRequired
+// }
+
+// export default Inputfield
