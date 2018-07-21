a0d0076538674fe7a04abac4ab8ad69100f7b26f
Kevin Hubert <>
2018-07-21 02:24:36 +0200

    Refactor: The Login-component does now implement the rendering-functionality (jsx-code) which was implemented in the Card-component before.
    Because the card won't be reused in the exact same look and feel i've decided to make individuell component instead of a too flexible and too complicated card-component which had to implement any possible layout.

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,23 +1,57 @@
 import React, { Component } from 'react'
 
-import Card from './Card'
+import Dropdown from 'react-dropdown'
+import 'react-dropdown/style.css'
 
 import ReactLogo from '../img/react.png'
+
+import Question from './Question'
 
 class Login extends Component {
 
   state = {
-    username: ''
+    selectedUser: undefined
+  }
+
+  handleClick = (event) => {
+    event.preventDefault()
+  }
+
+  handleDropdownChange = (item) => {
+    this.setState({
+      selectedUser: item
+    })
   }
 
   render() {
     return (
-      <Card title="Welcome to 'Would you Rather'" description='Login to continue'>
-        <div className='center-content'>
-          <h1 className="colorful">Sign In</h1>
-          <img src={ReactLogo} className='logo'/>
+
+      <div className='card small'>
+        {/* Header */}
+        <h1 className='center'>Welcome to 'Would you Rather'</h1>
+        <h2 className='center'>Select a user to continue</h2>
+        <hr />
+        {/* Body */}
+        <div className='flex-vertical flex-center user-selection '>
+          {this.state.selectedUser
+            ? <img src={'TODO:'} alt={`Avatar of ${this.state.selectedUser}`} className='avatar' />
+            : <img src={ReactLogo} alt='React Logo' className='logo' />
+          }
+          <Dropdown options={['val1', 'val2', 'val3']}
+            placeholder='Select a user'
+            onChange={this.handleDropdownChange}
+            value={this.state.selectedUser}
+          />
+          <button
+            className='m-t-2 m-b-2'
+            disabled={!this.state.selectedUser ? true : false}
+            onClick={(e) => this.handleClick(e)}
+          >
+            Select this User
+        </button>
         </div>
-      </Card>
+      </div>
+
     );
   }
 
