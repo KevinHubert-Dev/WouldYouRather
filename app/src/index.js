08af224b2205fa2a3a5a189d8749ad3365fd1a7d
Kevin Hubert <>
2018-07-21 02:31:44 +0200

    func: Used createStore and provider in index.js to give redux-functionality to the application. Also includes the create middlewares.

--- app/src/index.js
+++ app/src/index.js
@@ -3,4 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './redux/reducers/index'
+import middleware from './redux/middleware/index'
+
+console.log(reducer);
+console.log(reducer);
+console.log(reducer);
+
+const store = createStore(reducer, middleware)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider >,
+    document.getElementById('root'));
