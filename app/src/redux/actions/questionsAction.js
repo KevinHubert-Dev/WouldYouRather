f7efa6565c6c06e9e8b5b8d4a38e394e45a2bdd7
Kevin Hubert <>
2018-07-22 20:18:09 +0200

    feat: Added functionality in questionAction and questionReducer to create new questions and add votes to questions.

--- app/src/redux/actions/questionsAction.js
+++ app/src/redux/actions/questionsAction.js
@@ -1,6 +1,10 @@
+import * as API from '../../utils/_DATA'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
 export const QUESTIONS_LOADED = 'QUESTIONS_LOADED'
 export const QUESTION_ANSWERED = 'QUESTION_ANSWERED'
-export const QUESTION_CREATED = 'QUESTION_CREATED'
+export const QUESTION_CREATE = 'QUESTION_CREATE'
+
 
 export function questionsLoaded(questions) {
   return {
@@ -9,18 +13,46 @@ export function questionsLoaded(questions) {
   }
 }
 
-export function questionAnswered(question, userid) {
+export function questionAnswered(authedUser, questionid, answer) {
+  console.log("a")
   return {
-    type: QUESTIONS_LOADED,
-    question,
-    userid
+    type: QUESTION_ANSWERED,
+    questionid,
+    authedUser,
+    answer
   }
 }
 
+/*
+ * answer-param can be 'optionOne' or 'optionTwo'
+ */
+export function handleQuestionAnswer(authedUser, questionid, answer) {
+  return dispatch => {
+    dispatch(showLoading)
+    API._saveQuestionAnswer({ authedUser, qid: questionid, answer })
+      .then(() => {
+        dispatch(questionAnswered(authedUser, questionid, answer))
+        dispatch(hideLoading())
+      })
+  }
+}
 
-export function questionCreated(question) {
+export function handleQuestionCreate(optionOneText, optionTwoText, author) {
+  return dispatch => {
+    const question = { optionOneText, optionTwoText, author }
+    dispatch(showLoading())
+
+    API._saveQuestion(question)
+      .then(createdQuestion => {
+        dispatch(questionCreate(createdQuestion))
+        dispatch(hideLoading())
+      })
+  }
+}
+
+function questionCreate(question) {
   return {
-    type: QUESTIONS_LOADED,
+    type: QUESTION_CREATE,
     question
   }
 }
--- app/src/redux/reducers/questionReducer.js
+++ app/src/redux/reducers/questionReducer.js
@@ -1,9 +1,21 @@
 import * as QuestionsAction from '../actions/questionsAction'
 
-const questionReducer = (state = [], action) => {
+const questionReducer = (state = {}, action) => {
   switch (action.type) {
     case QuestionsAction.QUESTIONS_LOADED:
       return action.questions
+    case QuestionsAction.QUESTION_CREATE:
+      return { ...state, [action.question.id]: action.question }
+    case QuestionsAction.QUESTION_ANSWERED:
+      return {
+        ...state,
+        [action.questionid]: {
+          [action.answer]: [
+            ...state[action.questionid][action.answer].votes,
+            action.authedUser
+          ]
+        }
+      }
     default: return state;
   }
 }
