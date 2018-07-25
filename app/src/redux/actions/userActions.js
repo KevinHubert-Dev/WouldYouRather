539728b622fb283032c8b1005717bc4c18a6b51e
Kevin Hubert <>
2018-07-25 18:57:02 +0200

    feat: Add functionality to userReducer to handle question-creation and -answering by a user.
    This is required because the user-object contains information about all answered and created questions.
    Moreover i've removed not longer required code from userAction which was used for tracing.

--- app/src/redux/actions/userActions.js
+++ app/src/redux/actions/userActions.js
@@ -1,7 +1,6 @@
 export const USERS_LOADED = 'USERS_LOADED'
 
 export function usersLoaded(users) {
-  console.log("usersLoaded called")
   return {
     type: USERS_LOADED,
     users
--- app/src/redux/reducers/userReducer.js
+++ app/src/redux/reducers/userReducer.js
@@ -1,9 +1,33 @@
 import * as UserActions from '../actions/userActions'
+import * as QuestionActions from '../actions/questionsAction'
 
 const userReducer = (state = null, action) => {
   switch (action.type) {
-    case UserActions.USERS_LOADED: 
+    case UserActions.USERS_LOADED:
       return action.users
+    case QuestionActions.QUESTION_ANSWERED:
+      return {
+        ...state,
+        [action.authedUser]: {
+          ...state[action.authedUser],
+          answers: {
+            ...state[action.authedUser].answers,
+            [action.questionid]: action.answer
+          }
+        }
+      }
+
+    case QuestionActions.QUESTION_CREATE:
+      const updatedUser = {
+        [action.question.author]: {
+          ...state[action.question.author],
+          questions: [...state[action.question.author].questions, action.question.id]
+        }
+      }
+      return {
+        ...state,
+        ...updatedUser
+      }
     default: return state;
   }
 }
