8377913273cdfaaf905aad6805a827bef2faa849
Kevin Hubert <>
2018-07-25 23:52:48 +0200

    feat: Added all components to App.js which contains the react-router-functionality to display the required component.

--- app/src/App.js
+++ app/src/App.js
@@ -1,32 +1,61 @@
-import React, { Component } from 'react';
-import './App.css';
-import Navigation from './components/Navigation'
+import React, { Component, Fragment } from 'react';
+
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+import { connect } from 'react-redux'
+
+import Login from './components/Login'
 import Header from './components/Header'
+import Question from './components/Question'
+import NotFound from './components/NotFound'
+import Navigation from './components/Navigation'
+import ProfileList from './components/ProfileList'
+import PrivateRoute from './components/PrivateRoute'
+import QuestionList from './components/QuestionList'
+import HeaderProfile from './components/HeaderProfile'
+import CreateQuestion from './components/CreateQuestion'
+
+import LoadingBar from 'react-redux-loading'
+import * as SharedActions from './redux/actions/sharedAction'
+
+
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.dispatch(SharedActions.handleLoadInitialData())
+  }
+
   render() {
+
     return (
       <BrowserRouter>
-        <div className="App ">
-          <Header>
-            <div className='half'>
+        <Fragment>
+          <div className="App">
+            <Header>
               <Navigation />
+              <HeaderProfile />
+            </Header>
+            <LoadingBar />
+            <div className='flex-center'>
+              <Switch>
+                <Route path='/login' exact component={Login} />
+                <PrivateRoute path='/' exact component={QuestionList} />
+                <PrivateRoute path='/add' exact component={CreateQuestion} />
+                <PrivateRoute path='/leaderboard' exact component={ProfileList} />
+                <PrivateRoute path='/questions/:id' exact component={Question} />
+                <Route exact component={NotFound} />
+              </Switch>
             </div>
-          </Header>
-          <Switch>
-            <Route path='/' exact render={() => "/"} />
-            <Route path='/login' exact render={() => "Login"} />
-            <Route path='/new' exact render={() => "New"} />
-            <Route path='/profile' exact render={() => "Profile"} />
-            <Route path='/leaderboard' exact render={() => "Leaderboard"} />
-            <Route exact render={() => "Other (404)"} />
-          </Switch>
-        </div>
+          </div>
+        </Fragment>
       </BrowserRouter >
     );
   }
 }
 
-export default App
+function mapStateToProps() {
+  return {}
+}
+
+export default connect(mapStateToProps)(App)
