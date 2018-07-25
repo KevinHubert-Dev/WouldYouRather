b209668c42a8594ee75a66cd062a8d809316e900
Kevin Hubert <>
2018-07-25 20:20:50 +0200

    feat: "Profile"-component is now self-controlled which mean in fetch its data from redux and dispatch actions on its own.
    The only parameter which is now required to create a Profile is the id.

--- app/src/components/Profile.js
+++ app/src/components/Profile.js
@@ -1,46 +1,55 @@
 import React, { Component } from 'react'
 
+import { connect } from 'react-redux'
+
 import Dropdown from 'react-dropdown'
 import 'react-dropdown/style.css'
 
 import PropTypes from 'prop-types'
 
+import * as QuestionsActions from '../redux/actions/questionsAction'
 
-class Question extends Component {
+class Profile extends Component {
 
   render() {
-    const { name, avatarURL, questionsAnswered, questionsAsked } = this.props
+    const { profile } = this.props
+    const { name, avatarURL, answers, questions } = profile
+    const questionsAnswered = Object.keys(answers).length
+    const questionsAsked = questions.length
 
     return (
-      <div className='card profile-card flex-horizontal'>
-        {/* Header */}
-        <div className='flex-1 flex-center'>
-          <img className='profile-avatar'
-            src={avatarURL}
-            alt={`Avatar of ${name}`}
-          />
-        </div>
-        <div className='flex-2'>
-          <h1 className='colorful'>{name} - Profile</h1>
-          <h3>Asked Questions: {questionsAnswered}</h3>
-          <h3>Created Questions: {questionsAsked}</h3>
-        </div>
-        <div className='flex-1 flex-center'>
-          <div className='score-bubble flex-center flex-vertical'>
-            <h1>{questionsAnswered + questionsAsked}</h1>
-            <h2>Points</h2>
+      profile
+        ? <div className='card flex-horizontal flex-center'>
+          {/* Header */}
+          <div className='flex-1 flex-center'>
+            <img className='avatar round'
+              src={avatarURL}
+              alt={`Avatar of ${name}`}
+            />
+          </div>
+          {/* Profile statistic */}
+          <div className='flex-2'>
+            <h1 className='blue'>{name} - Profile</h1>
+            <h3>Answered Questions: {questionsAnswered}</h3>
+            <h3>Created Questions: {questionsAsked}</h3>
+          </div>
+          <div className='flex-1 flex-center'>
+            <div className='score-bubble flex-center flex-vertical'>
+              <h1>{questionsAnswered + questionsAsked}</h1>
+              <h2>Points</h2>
+            </div>
           </div>
         </div>
-      </div>
+        : <p>This profile does not exists</p>
     );
   }
 }
 
-Question.propTypes = {
-  name: PropTypes.string.isRequired,
-  avatarURL: PropTypes.string.isRequired,
-  questionsAnswered: PropTypes.number.isRequired,
-  questionsAsked: PropTypes.number.isRequired
+
+function mapStateToProps({ users }, { id }) {
+  return {
+    profile: users[id]
+  }
 }
 
-export default Question
+export default connect(mapStateToProps)(Profile)
